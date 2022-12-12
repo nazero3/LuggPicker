cb4e0229629f334a6e5894eb0396d3fa3d99224b
nazero3 <nazerrabah1994@gmail.com>
2022-12-12 19:24:39 +0300

    authen done

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,40 +1,39 @@
-import Link from "next/link";
-import { useSession ,signOut} from "next-auth/client";
+import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/client';
 
-import classes from "./main-navigation.module.css";
+import classes from './main-navigation.module.css';
 
 function MainNavigation() {
   const [session, loading] = useSession();
-  //console.log(loading);
-  //console.log(session);
 
-  function logoutHandler(){
+  function logoutHandler() {
     signOut();
   }
+
   return (
     <header className={classes.header}>
-      <Link href="/">
-        <div className={classes.logo}>LuggPicker</div>
+      <Link href='/'>
+        
+          <div className={classes.logo}>Next Auth</div>
+        
       </Link>
       <nav>
         <ul>
           {!session && !loading && (
             <li>
-              <Link href="/auth">Login</Link>
+              <Link href='/auth'>Login</Link>
             </li>
           )}
-
-          { 
+          {session && (
             <li>
-              <Link href="/profile">Profile</Link>
+              <Link href='/profile'>Profile</Link>
             </li>
-          }
-
-          { 
+          )}
+          {session && (
             <li>
-              <button onClick={logoutHandler}>Log out</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
-          }
+          )}
         </ul>
       </nav>
     </header>
