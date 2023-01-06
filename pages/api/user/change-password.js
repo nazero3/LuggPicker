1b36da971817999f3f375f2aeac4b5989d684971
nazero3 <nazerrabah1994@gmail.com>
2023-01-06 10:56:13 +0300

    a

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -8,12 +8,12 @@ async function handler(req, res) {
     return;
   }
 
-  const session = await getSession({ req: req });
+  // const session = await getSession({ req: req });
 
-  if (!session) {
-    res.status(401).json({ message: 'Not authenticated!' });
-    return;
-  }
+  // if (!session) {
+  //   res.status(401).json({ message: 'Not authenticated!' });
+  //   return;
+  // }
 
   const userEmail = session.user.email;
   const oldPassword = req.body.oldPassword;
